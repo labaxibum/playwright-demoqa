71d23d2b61190700dfcd0e5a8d80e16d28d2255e
anhlet4 <aris.nttest03@nashtechglobal.com>
2024-04-15 17:39:42 +0700

    add fixture pages

--- src/fixtures/pageFixtures.js
+++ src/fixtures/pageFixtures.js
@@ -2,6 +2,12 @@ import {test as base} from '@playwright/test';
 import { Homepage } from '../POM/homepage';
 import { BookStorePage } from '../POM/bookstore_page';
 
-const test = base.extend<{homepage:Homepage, bookStorePage:BookStorePage}>({
-    
+const test = base.test.extend({
+    homepage: async ({page} , use)=>{
+        await use(new Homepage(page));
+    },
+
+    bookstorepage: async ({page} , use)=>{
+        await use(new BookStorePage(page));
+    }
 })
