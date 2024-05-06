bfaa56068c64c5deaf22778496acf257720f813c
labaxibum <labaxibum216@gmail.com>
2024-05-06 19:59:12 +0700

    add jsonschema

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  //timeout: 5 * 60 * 10000,
+  timeout: 5 * 60 * 10000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
--- src/POM/bookstore_page.js
+++ src/POM/bookstore_page.js
@@ -1,38 +1,38 @@
-import { BasePage } from './basepage';
-
+import { BasePage } from "./basepage";
+const { Element } = require("../core/element/elementUtils");
 export class BookStorePage extends BasePage {
-    constructor(page) {
-        super(page);
-        this.getLoginButton = page.getByRole('button', { name: 'Login' });
-        this.getSearchField = page.getByPlaceholder('Type to search');
-        this.getLogoutButton = page.getByRole('button', { name: 'Log out' });
-        this.getSearchResultInTable = page.locator('//span[contains(@id,"see-book")]/a');
-        this.getUserNameValue = page.locator('#userName-value');
-    }
+  constructor(page) {
+    super(page);
+    this.getLoginButton = page.getByRole("button", { name: "Login" });
+    this.getSearchField = page.getByPlaceholder("Type to search");
+    this.getLogoutButton = page.getByRole("button", { name: "Log out" });
+    this.getSearchResultInTable = new Element(
+      'xpath=//span[contains(@id,"see-book")]/a'
+    );
+    this.getUserNameValue = page.locator("#userName-value");
+  }
 
-    async clickLoginButtonInBookStore() {
-        await this.getLoginButton.click();
-    }
+  async clickLoginButtonInBookStore() {
+    await this.getLoginButton.click();
+  }
 
-    async inputIntoSearchField(searchText) {
-        await this.getSearchField.fill(searchText);
-    }
+  async inputIntoSearchField(searchText) {
+    await this.getSearchField.fill(searchText);
+  }
 
-    async getResultFromSearchResult() {
-        return await this.getSearchResultInTable.allInnerTexts();
-    }
+  async getResultFromSearchResult() {
+    await getSearchResultInTable.getText;
+  }
 
-    async clearSearchField() {
-        await this.getSearchField.clear();
-    }
+  async clearSearchField() {
+    await this.getSearchField.clear();
+  }
 
-    async waitForLogoutAppear() {
-        await page.waitForSelector(getLogoutButton);
-    }
+  async waitForLogoutAppear() {
+    await page.waitForSelector(getLogoutButton);
+  }
 
-    async getUsernameValue() {
-        return await this.getUserNameValue.innerText();
-    }
-
-
+  async getUsernameValue() {
+    return await this.getUserNameValue.innerText();
+  }
 }
